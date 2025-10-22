46ceb555de9095463018f2c4cea34f364779d35f
Denys Shapovalov <desha@smart-it.com>
2025-10-22 16:14:27 +0300

    Added ApplicationDbContextFactory that allow dotnet ef to create database without secrets from KeyVault

---
+++ app/backend/NewerDown.Infrastructure/Data/Factory/ApplicationDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace NewerDown.Infrastructure.Data.Factory;
+
+public class ApplicationDbContextFactory: IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings_DefaultConnection");
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
